a349e90218fca642351abc1d921424fa39cccb8b
CalebLG <gentrcal000@warren.k12.in.us>
2021-08-24 12:16:22 -0400

    Merge remote-tracking branch 'origin/master'

--- 1.5 Practice Problems/src/CalculatingDistance.java
+++ 1.5 Practice Problems/src/CalculatingDistance.java
@@ -2,8 +2,18 @@ public class CalculatingDistance {
 
     /*
     Create a program that calculates a vehicle's distance based on speed traveled and time traveled.
-
     Example: 55 mph for 45 minutes -> 41.25 miles
 
     */
+
+    public static void main(String[] args) {
+
+        double hours = 0.75;
+        double mph = 55;
+
+        double distance = hours * mph;
+
+        System.out.println("The total distance traveled is " + distance);
+
+    }
 }
